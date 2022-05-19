05f265c5dd529dad603a99de7b87396a3e9fd8e3
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-05-19 13:46:34 +0200

    exercise functions

---
+++ functions/main.py
@@ -0,0 +1,27 @@
+# Do not modify these lines
+__winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
+__human_name__ = 'functions'
+
+# Add your code after this line
+def greet(name):
+    return f"Hello, {name}!"
+greet("Bob")
+
+def add(num_1, num_2, num_3):
+    return num_1 + num_2 + num_3
+
+add(5,3,2)
+
+def positive(num):
+    return num > 0
+
+positive(50)
+positive(-50)
+positive(0)
+
+def negative(num):
+    return num < 0
+
+negative(50)
+negative(-50)
+negative(0)
