3bda36a92ff5a8112f76715d9489f283548f2663
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-05-19 10:34:18 +0200

    processing feedback

--- strings/main.py
+++ strings/main.py
@@ -3,39 +3,35 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-player_1 = "Ruud Gullit"
-player_2 = "Marco van Basten"
+scorer_name_0 = "Ruud Gullit"
+scorer_name_1 = "Marco van Basten"
 
-goal_1 = "32"
-goal_2 = "54"
+goal_0 = 32
+goal_1 = 54
 
-scorer_1 = f"{player_1} {goal_2}"
-scorer_2 = player_2 + " " + goal_2
-# print(scorer_2)
+scorer_0 = f"{scorer_name_0} {goal_0}" # manier 1 om samen te voegen
+scorer_1 = scorer_name_1 + " " + str(goal_1) # manier 2
+scorers = scorer_0 + ", " + scorer_1
+# print(scorer_total)
 
-report = f"{player_1} scored in the {goal_1}nd minute\n{player_2} scored in the {goal_2}th minute."
+report = f"{scorer_name_0} scored in the {goal_0}nd minute\n{scorer_name_1} scored in the {goal_1}th minute"
 # print(report)
 
 # splitting first and last name, option 1
-player_3 = "Frank Rijkaard"
-splitted_player_3 = player_3.split()
-first_name_3 = splitted_player_3[0]
-last_name_3 = splitted_player_3[1]
-# print(first_name_3)
-# print(last_name_3)
+player_2 = "Frank Rijkaard"
+splitted_player_2 = player_2.split()
+first_name_2 = splitted_player_2[0]
+last_name_2 = splitted_player_2[1]
 
 # splitting first and last name, option 2
-player_4 = "Hans van Breukelen"
-x = player_4.find(" ")
-first_name_4 = player_4[:x]
-last_name_4 = player_4[x+1:]
-# print(first_name_4)
-# print(last_name_4)
-name_short_4 = first_name_4[0] + ". " + last_name_4
-# print(name_short_4)
+player = "Hans van Breukelen"
+x = player.find(" ")
+first_name = player[:x]
+last_name = player[x+1:]
+name_short = first_name[0] + ". " + last_name
+last_name_len = len(last_name)
 
-chant = (first_name_4 + "!") * len(first_name_4)
-print(chant)
 
-print(2 != 3)
-print(2 != 2)
+chant = (first_name + "! ") * (len(first_name)-1) + (first_name + "!")
+bad_chant = (first_name + "! ") * len(first_name)
+good_chant = chant[-1] != " "
