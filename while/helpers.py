c160950dad6f67c6348c2b2e728e1c73545eac3b
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2023-01-02 13:51:33 +0100

    part 1

--- while/helpers.py
+++ while/helpers.py
@@ -3,6 +3,7 @@ import os
 import random
 
 
+
 def random_koala_fact():
     module_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data_fp = open(os.path.join(module_path, "facts.json"), "r")
--- while/main.py
+++ while/main.py
@@ -3,7 +3,17 @@ from helpers import random_koala_fact
 __winc_id__ = "c0dc6e00dfac46aab88296601c32669f"
 __human_name__ = "while"
 
+
+def unique_koala_facts(number):
+    if number > 1000:
+        return print("try less")
+    while number > 0:
+        print("___", random_koala_fact())
+        number -= 1
+
 # This block is only executed if this script is run directly (python main.py)
 # It is not run if you import this file as a module.
-if __name__ == "__main__":
-    print(random_koala_fact())
+# if __name__ == "__main__":
+    # print(random_koala_fact())
+
+unique_koala_facts(15)
