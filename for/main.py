5847eff2ce92b80faa235b7329e579bf321f74e8
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2023-01-03 14:43:32 +0100

    fix typo length & fix check

--- for/main.py
+++ for/main.py
@@ -8,58 +8,57 @@ __human_name__ = "for"
 
 """ Write your functions here. """
 
-def shortest_names():
-    countries_lenght = {}
-    sorted_countries_by_lenght = {}
+def shortest_names(countries):
+    countries_length = {}
+    sorted_countries_by_length = {}
     shortest_countries = []
     for countrie in countries:
-        countries_lenght[countrie] = len(countrie);
-    sorted_countries_by_lenght = sorted(countries_lenght.items(), key=lambda x:x[1]);
-    min_lenght = sorted_countries_by_lenght[0][1]
-    for sorted_countrie in sorted_countries_by_lenght:
-        if sorted_countrie[1] == min_lenght:
+        countries_length[countrie] = len(countrie);
+    sorted_countries_by_length = sorted(countries_length.items(), key=lambda x:x[1]);
+    min_length = sorted_countries_by_length[0][1]
+    for sorted_countrie in sorted_countries_by_length:
+        if sorted_countrie[1] == min_length:
             shortest_countries.append(sorted_countrie[0])  
     # print("kortste landen:", shortest_countries[0:2])
     return shortest_countries
 
-def most_vowels():
+def most_vowels(countries):
     countries_by_vowels = {}
     sorted_countries_by_vowels = {}
     countries_most_vowels = []
-    # vowels = ["a", "e", "i", "o", "u"]
     for countrie in countries:
         num_vowels = 0
         for char in countrie:
-            if char in "aeiouAEIOU":
+            if char.lower() in "aeiou":
                 num_vowels += 1
         countries_by_vowels[countrie] = num_vowels
         sorted_countries_by_vowels = sorted(countries_by_vowels.items(), key=lambda x:x[1], reverse=True);
     for sorted_countrie_by_vowels in sorted_countries_by_vowels:
         countries_most_vowels.append(sorted_countrie_by_vowels[0])
-    # print("meeste klinkers:", countries_most_vowels[0:2])
-    return countries_most_vowels
+    # print("meeste klinkers:", countries_most_vowels[0:3])
+    return countries_most_vowels[0:3]
 
 
 # volgorde weinig -> z, q, j, x, k, v b p g
 countries_alphabet=[]
 
-def add_new_country(next_char):
+def add_new_country(next_char, countries):
     for countrie in countries:
         for char in countrie:
             if char.lower() == next_char:
                 countries_alphabet.append(countrie)
                 return
 
-def check_in_countries_alphabet(next_char):
+def check_in_countries_alphabet(next_char, countries):
     for countrie in countries_alphabet:
         for char in countrie:
             if char.lower() == next_char:
                 return
-    add_new_country(next_char)
+    add_new_country(next_char, countries)
 
-def alphabet_set():
+def alphabet_set(countries):
     for next_char in "zqjxkvbpgacdefhilmnorstuwy":
-        check_in_countries_alphabet(next_char)
+        check_in_countries_alphabet(next_char, countries)
     return countries_alphabet
     # print("countries_alphabet:", countries_alphabet, len(countries_alphabet))
 
@@ -67,9 +66,9 @@ def alphabet_set():
 # It is not run if it is imported as a module: `from main import *`
 if __name__ == "__main__":
     countries = get_countries()
-    shortest_names()
-    most_vowels()
-    alphabet_set()
+    shortest_names(countries)
+    most_vowels(countries)
+    alphabet_set(countries)
 
     """ Write the calls to your functions here. """
 
